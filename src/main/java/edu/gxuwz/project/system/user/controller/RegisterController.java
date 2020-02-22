ff551fe517afbc75f112f7db9c0846f7b0e63a72
429829320@qq.com <429829320@qq.com>
2020-02-22 14:54:18 +0800

    ok

--- src/main/java/edu/gxuwz/project/system/user/controller/RegisterController.java
+++ src/main/java/edu/gxuwz/project/system/user/controller/RegisterController.java
@@ -137,6 +137,7 @@ public class RegisterController extends BaseController {
                 }else{
                     u.setRoleIds(new Long[]{102L});
                 }
+
                 // 新增用户与角色管理
                 userService.insertUserRole(u);
                 userService.updateUserInfo(u);
